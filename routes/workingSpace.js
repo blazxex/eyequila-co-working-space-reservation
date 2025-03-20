7f97a4eaf0d99c03c7f47db1ba75a19d67864223
paopeaw <paopeaw8246@gmail.com>
2025-03-20 23:31:23 +0700

    Feat : getRooms

--- routes/workingSpace.js
+++ routes/workingSpace.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { getSpaces, getSpace } = require("../controllers/workingSpace");
-const room = require('../routes/room.js')
+const roomRouter = require('../routes/room.js')
 
 const router = express.Router();
 
-router.route('/:SpaceId/rooms/', room);
+router.use('/:spaceId/rooms/', roomRouter);
 
 router.route('/').get(getSpaces);
 router.route('/:id').get(getSpace);
