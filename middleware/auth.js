497eed2ab094f72a3b7d44d50949c8c2c5d62ba4
Blazxex <armygm162@gmail.com>
2025-04-08 23:26:40 +0700

    feat: setup logout route

--- middleware/auth.js
+++ middleware/auth.js
@@ -27,7 +27,7 @@ exports.protect = async (req, res, next) => {
     // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id);
-    console.log(req.user);
+    // console.log(req.user);
     next();
   } catch (err) {
     console.log(err.stack);
--- routes/auth.js
+++ routes/auth.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", logout);
 
 module.exports = router;
