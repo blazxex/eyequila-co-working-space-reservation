93a30f5bd73166afae251bdb80aba8834c2b1e39
Blazxex <armygm162@gmail.com>
2025-04-25 16:58:04 +0700

    fix: add validator for reservation

--- routes/reservation.js
+++ routes/reservation.js
@@ -13,18 +13,20 @@ const {
 } = require("../controllers/reservation.js");
 const { protect } = require("../middleware/auth.js");
 const { getMe } = require("../controllers/user.js");
-const { reservationValidator } = require("../middleware/validator/reservation.validator.js");
+const {
+  reservationValidator,
+} = require("../middleware/validator/reservation.validator.js");
 const validate = require("../middleware/validator/validate.js");
 
 router
   .route("/")
   .get(protect, getReservations)
-  .post(protect, createReservation);
+  .post(protect, reservationValidator, validate, createReservation);
 router.get("/verify", verifyQRCode);
 router
   .route("/:reservationId")
   .get(protect, getReservation)
-  .put(protect, editReservation)
+  .put(protect, reservationValidator, validate, editReservation)
   .delete(protect, cancelReservation);
 router.get("/:reservationId/qr", protect, getReservationQR);
 
